7e7ab116cf37cc5e46ed9ed54394cac855cbffba
itgkj0x <itgk.j0x@gmail.com>
2025-05-05 17:02:01 +0900

    色変更(無効化できない)

--- src/apply.js
+++ src/apply.js
@@ -1,28 +1,44 @@
 //import pgch from "count.js";
 //import tdli from "tdli.js";
-const settings = JSON.parse(localStorage.getItem("settings"));
-//settingsの呼び出し
 
-window.onload = apply();
-//apply関数の実行
+// 設定を取得してからapplyを実行
+// 取得したsettingsはコールバック内でしか使えないからここで値を渡す
 
-function apply(){
+
+window.onload = function() {
+    chrome.storage.local.get("settings", (result) => {
+        const settings = result.settings;
+        if (!settings) return;
+        apply(settings);
+    });
+};
+
+function apply(settings){
     //if(settings[1]){
     //    pgch();
     //};
     //if(settings[2]){
     //    tdli();
     //};
-    if(settings[2]){
-        clch();
-    };
-};
+    if(settings[3]){
+        clch(settings);
+    }
+    else{
+        clchrem();
+    }
+}
 
-function clch() {
+function clch(settings) {
     const style = document.createElement('style');
-    const css = 'body { background:'+settings[3]+'!important; }';
+    const css = 'body { background:' + settings[3] + ' !important; }';
     style.textContent = css;
     console.log(css);
     document.head.appendChild(style);
-  };
+}
 //styleタグを作りcss書きbgを上書きする
+
+function clchrem(){
+    const style = document.createElement('style');
+    style.remove();
+}
+//html内のstyleを削除?この状態だと全部消してしまう
--- src/save.js
+++ src/save.js
@@ -2,13 +2,16 @@ const form = document.getElementById("form");
 // formとしてsubmit時の動作を適用しないとほかのsubmitボタンが出てきたときに困る
 
 // localStorageから設定を取得し、フォームに反映
-const settings = JSON.parse(localStorage.getItem("settings"));
-if (settings) {
-    document.getElementById("ch1").checked = settings[0];
-    document.getElementById("ch2").checked = settings[1];
-    document.getElementById("ch3").checked = settings[2];
-    document.getElementById("clInput").value = settings[3];
-}
+const settings = chrome.storage.local.get("settings",(result)=>{
+    const settings = result.settings;
+    if (settings) {
+        document.getElementById("ch1").checked = settings[0];
+        document.getElementById("ch2").checked = settings[1];
+        document.getElementById("ch3").checked = settings[2];
+        document.getElementById("clInput").value = settings[3];
+    }
+});
+
  // submit時リロードを防ぎsave()を実行させる
 form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -22,7 +25,8 @@ function save() {
     const in3 = document.getElementById("ch3").checked;
     const in4 = document.getElementById("clInput").value;
     const settings = [in1, in2, in3, in4];
-    localStorage.setItem("settings", JSON.stringify(settings));
+    chrome.storage.local.set({settings:settings});
 }
 // 変数をsubmitされる前に定義してしまうと変更が適用されないのに注意する
 // だからsaveのところと設定を呼び出すとこで長文書かなきゃいけなくなってる
+
